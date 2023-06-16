980da3d7423feb879c9f2caf863a06427dde928f
Sergey <saizotov1@gmail.com>
2023-06-16 14:28:45 +0500

    decision of task 36

--- task_36.py
+++ task_36.py
@@ -17,7 +17,8 @@
 # 6 12 18 24 30 36
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
-for i in a:
-print(*[f'{x:>3}' for x in i])
+    func_1 = [[operation(i, j) for i in range(1, num_rows + 1) for j in range(1, num_columns + 1)]]
+for i in func_1:
+    for x in i:
+    print(*[f'{x:>3}'])
 print_operation_table(lambda x, y: x * y)
